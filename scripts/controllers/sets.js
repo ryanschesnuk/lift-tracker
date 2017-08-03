8500be88aa87a9d83ff092d97531025c487656ec
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-08-02 21:25:56 -0600

    Create 'addSet' functionality

--- scripts/controllers/sets.js
+++ scripts/controllers/sets.js
@@ -2,10 +2,16 @@
 
 angular.module("liftTrackerApp")
 .controller('setCtrl', function($scope){
-    $scope.sets = [
-      {"reps": 5, "weight": 135},
-      {"reps": 5, "weight": 135},
-      {"reps": 5, "weight": 135},
-    ];
+
+  $scope.addSet = function (){
+    let newSet = {"reps": 5, "weight": 135};
+    $scope.sets.push(newSet);
+  };
+
+  $scope.sets = [
+    {"reps": 5, "weight": 135},
+    {"reps": 5, "weight": 135},
+    {"reps": 5, "weight": 135},
+  ];
 
 });
