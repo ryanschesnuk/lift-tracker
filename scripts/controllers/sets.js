b8855b280ce58fca9adb7a992c8b63452a5471eb
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-08-02 22:53:00 -0600

    Add 'remove set' functionality. Rename 'directives/sets.js' to 'directives/sets-dir.js' to avoid confusion

--- scripts/controllers/sets.js
+++ scripts/controllers/sets.js
@@ -8,6 +8,10 @@ angular.module("liftTrackerApp")
     $scope.sets.push(newSet);
   };
 
+  $scope.deleteSet = function(set, $index) {
+    $scope.sets.splice($index, 1);
+  };
+
   $scope.sets = [
     {"reps": 5, "weight": 135},
     {"reps": 5, "weight": 135},
