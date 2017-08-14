21694a45d02c73309fd02586e4745454b4101b0d
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-08-13 23:00:29 -0600

    Create Sun-Sat weekly calendar that is automatically generated from one date.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -3,14 +3,111 @@
 angular.module("liftTrackerApp")
 .controller('mainCtrl', function($scope) {
 
-  // Define day names in schedule using ng-repeat
-  $scope.days = [
-    {"name": "Monday"},
-    {"name": "Tuesday"},
-    {"name": "Wednesday"},
-    {"name": "Thursday"},
-    {"name": "Friday"},
-    {"name": "Saturday"},
-    {"name": "Sunday"}
-  ];
+  $scope.days = [];
+
+
+  const currentDate = new Date();
+
+
+
+
+  // Print day name
+  function printDateName(day, month, date, year) {
+    let dayLabels = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+    let monthLabels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'];
+
+
+    return {
+        "dayName": dayLabels[day],
+        "monthName": monthLabels[month],
+        "date": date,
+        "year": year
+      };
+  }
+
+  function createCurrentWeek(date) {
+    const daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // Account for Leap year
+    if ((date.getFullYear() % 4 == 0 && date.getFullYear() % 100 != 0) || date.getFullYear() % 400 == 0){
+      daysInMonths[1] = 29;
+    }
+
+    let dayPosition = date.getDay();
+    let monthNumber = date.getMonth();
+    let dayNumber = date.getDate();
+    let year = date.getFullYear();
+
+    let today = new Date();
+
+    // First Part of Week
+
+    for (var i=dayPosition; i>=0; i--){
+
+      if (dayNumber < 1) {
+        monthNumber--;
+
+        if (monthNumber < 0) {
+          monthNumber = 11;
+          year--;
+          }
+
+        dayNumber = daysInMonths[monthNumber];
+
+      }
+
+      $scope.days.unshift(printDateName(i, monthNumber, dayNumber, year));
+      // if (monthNumber === today.getMonth() && dayNumber === today.getDate() && year === today.getFullYear()){
+      //   $scope.isToday = true;
+      // } else {
+      //   $scope.isToday = false;
+      // }
+      dayNumber--;
+    }
+
+    // Second Part of Week
+    if ( dayPosition < 6) {
+      monthNumber = date.getMonth();
+      dayNumber = date.getDate() + 1;
+      year = date.getFullYear();
+
+    for (var j=dayPosition+1; j<=6; j++) {
+      if (dayNumber > daysInMonths[monthNumber]) {
+        monthNumber++;
+        dayNumber = 1;
+
+        if (monthNumber > 11) {
+          monthNumber = 0;
+          year++;
+        }
+      }
+      $scope.days.push(printDateName(j, monthNumber, dayNumber, year));
+      dayNumber++;
+    }
+  }
+}
+
+createCurrentWeek(currentDate);
+
+
 });
